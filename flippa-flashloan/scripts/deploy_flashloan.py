afd1c6c06a639d4e9e2812b06045eebe85812c40
mmsaki <meek10x@gmail.com>
2022-09-17 05:20:33 -0500

    [update] use address provider

--- flippa-flashloan/scripts/deploy_flashloan.py
+++ flippa-flashloan/scripts/deploy_flashloan.py
@@ -1,11 +1,11 @@
 from brownie import accounts, config, network, interface, FlashloanAttacker
 
-ap = interface.IPoolAddressesProvider(config["networks"][network.show_active()]["PoolAddressesProvider-Optimistic"])
+address_provider = interface.IPoolAddressesProvider(config["networks"][network.show_active()]["PoolAddressesProvider-Optimistic"])
 tx_url = "https://goerli-optimism.etherscan.io/address/{}"
 
 def main():
     acct = accounts.add(config["wallets"]["from_key"])  
-    flashloan = FlashloanAttacker.deploy(ap, {'from': acct})
+    flashloan = FlashloanAttacker.deploy(address_provider, {'from': acct})
     print(f"Deployed FlashloanAttacker Contract")
     print("View your contract here: " + tx_url.format(flashloan.address))
     return flashloan.tx.info()
