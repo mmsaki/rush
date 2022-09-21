c2583f523590e9263169d3279c29141c89e26510
mmsaki <meek10x@gmail.com>
2022-09-21 01:28:12 -0500

    get weth script

--- flippa-flashloan/scripts/get_weth.py
+++ flippa-flashloan/scripts/get_weth.py
@@ -1,15 +1,24 @@
 from brownie import accounts, config, network, interface
 
-amount = 100
 tx_url = "https://goerli-optimism.etherscan.io/tx/{}"
 
 def main():
     get_weth()
 
 def get_weth():
+    amount = 4 * 10 ** 18
     acct = accounts.add(config["wallets"]["from_key"]) 
     weth = interface.IWETH(config["networks"][network.show_active()]['WETH-TestnetMintableERC20'])
     tx = weth.deposit({"from": acct, "value": amount})
     print(f"Received {amount} WETH")
+    print("View your tx here: " + tx_url.format(tx.txid))
+    return tx.info()
+
+def withdraw_eth():
+    amount = 10 
+    acct = accounts.add(config["wallets"]["from_key"]) 
+    weth = interface.IWETH(config["networks"][network.show_active()]['WETH-TestnetMintableERC20'])
+    tx = weth.withdraw(amount, {"from": acct })
+    print(f"Received {amount} ETH")
     print("View your tx here: " + tx_url.format(tx.txid))
     return tx.info()
