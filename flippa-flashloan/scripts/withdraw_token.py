4725a3f80f9b4d6462a2237b0032b5fee4775c5f
mmsaki <meek10x@gmail.com>
2022-09-21 01:19:02 -0500

    token withdraw script

---
+++ flippa-flashloan/scripts/withdraw_token.py
@@ -0,0 +1,9 @@
+from scripts.deploy_flashloanhelper_functions import get_token, get_lending_pool, get_account
+
+# Enter string of token i.e "DAI", "USDC" "USDT" , see config.yaml for more
+def withdraw_token(_token, amount):
+    account = get_account()
+    lending_pool = get_lending_pool()
+    token = get_token(_token)
+    tx = lending_pool.withdraw(token.address, amount, account, {"from": account})
+    return tx.info()
