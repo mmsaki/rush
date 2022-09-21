d152115abbc74d039aaab82e34905582200993ce
mmsaki <meek10x@gmail.com>
2022-09-21 01:27:51 -0500

    flashloan script update

--- flippa-flashloan/scripts/run_flashloan.py
+++ flippa-flashloan/scripts/run_flashloan.py
@@ -1,16 +1,44 @@
-from brownie import accounts, config, network, interface, FlashloanAttacker
+from brownie import accounts, config, network, interface, FlashLoanReceiver, SimpleFlashLoanReceiver
+from scripts.helper_functions import get_address_provider, get_token, get_account
 
-min_weth_balance = 5
+
 tx_url = "https://goerli-optimism.etherscan.io/tx/{}"
 
+pool = interface.IPool(get_address_provider().getPool())
+
+flashloan_receiver = FlashLoanReceiver[len(FlashLoanReceiver) -1]
+
+dai = get_token("DAI")
+usdc = get_token("USDC")
+usdt = get_token("USDT")
+susd = get_token("SUSD")
+weth = get_token("WETH")
+wbtc = get_token("WBTC")
+aave = get_token("AAVE")
+link = get_token("LINK")
+
+dai_amount = 10000 * 10** dai.decimals()
+usdc_amount = 10000 * 10** usdc.decimals()
+usdt_amount = 10000 * 10** usdt.decimals()
+susd_amount = 10000 * 10** susd.decimals() 
+weth_amount = 100 * 10** weth.decimals()
+wbtc_amount = 10 * 10** wbtc.decimals()
+aave_amount = 1000 * 10** aave.decimals()
+link_amount = 10000 * 10** link.decimals()
+
+def flip_a_loan():
+    acct = get_account()
+    receiver = flashloan_receiver
+    assets = [dai, usdc, usdt, susd, weth, wbtc, aave, link]
+    amounts = [dai_amount, usdc_amount, usdt_amount, susd_amount, weth_amount, wbtc_amount, aave_amount, link_amount]
+    interest = [0, 0, 0, 0, 0, 0, 0, 0]
+    behalf_of = receiver
+    params = receiver.address
+    referral = 0
+    tx = pool.flashLoan(receiver, assets, amounts, interest, behalf_of, params, referral, {"from": acct})
+    tx.wait(3)
+    # print(f"Congrats! You have flipped a flashloan. Check it out! {tx_url.format(tx.txid)}")
+    return tx.events
 
 def main():
-    acct = accounts.add(config["wallets"]["from_key"])
-    flashloan = FlashloanAttacker[len(FlashloanAttacker) -1]
-    weth = interface.IWETH(config["networks"][network.show_active()]['WETH-TestnetMintableERC20'])
-    # weth.balanceOf(flashloan) < min_weth_balance
-    weth.approve(flashloan, min_weth_balance, {'from':acct})
-    weth.transferFrom(acct, flashloan, min_weth_balance, {'from': acct})
-    tx = flashloan.supplyAsset(weth, min_weth_balance, {'from': acct})
-    print("You supplied an Asset! View your tx here: " + tx_url.format(tx.txid))
-    return tx.info()
+    flip_a_loan()
