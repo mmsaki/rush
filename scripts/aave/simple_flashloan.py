a6d83e685f2b0fb7e5250477bae23830b4ec738c
Meek <meek10x@gmail.com>
2023-01-06 04:47:24 -0600

    [fix] using explicit str address and removing wait confirmation

--- scripts/aave/simple_flashloan.py
+++ scripts/aave/simple_flashloan.py
@@ -1,29 +1,23 @@
 from brownie import accounts, config, network, interface, RushSimpleFlashLoan
 from scripts.aave.helper_functions import get_address_provider, get_token, get_account
-import web3
 
-tx_url = "https://goerli-optimism.etherscan.io/tx/{}"
 
+tx_url = "https://goerli.etherscan.io/tx/{}"
 pool = interface.IPool(get_address_provider().getPool())
-
 flashloan_receiver = RushSimpleFlashLoan[len(RushSimpleFlashLoan) -1]
-
 usdc = get_token("USDC")
-
-usdc_amount = 1000 * 10** usdc.decimals()
+usdc_amount = 1_000_000 * 10** usdc.decimals()
 
 
 def run_flashloan():
     dev = get_account()
-    receiver = flashloan_receiver
-    assets = usdc
+    receiver = flashloan_receiver.address
+    assets = usdc.address
     amount = usdc_amount
     # 'flashLoanSimple': "0x42b0b77c"
     params = "0x42b0b77c"
     referral = 0
     tx = pool.flashLoanSimple(receiver, assets, amount, params, referral, {"from": dev})
-    
-    tx.wait(3)
     print(f"Congrats! You have flipped a flashloan. Check it out! {tx_url.format(tx.txid)}")
     return tx
 
