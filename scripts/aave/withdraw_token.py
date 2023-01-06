a856cc88a61c7f3c4d45aa6d1b06c9f82282ede5
Meek <meek10x@gmail.com>
2023-01-06 05:37:08 -0600

    [fix] linting

--- scripts/aave/withdraw_token.py
+++ scripts/aave/withdraw_token.py
@@ -5,12 +5,14 @@ from scripts.aave.helper_functions import get_token, get_lending_pool, get_accou
 token_name = "AAVE"
 amount = 4198416377744149752773
 
+
 def withdraw_token():
     dev = get_account()
     lending_pool = get_lending_pool()
     token = get_token(token_name)
     tx = lending_pool.withdraw(token.address, amount, dev, {"from": dev})
-    return  tx
+    return tx
+
 
 def main():
     withdraw_token()
